ce96e0002b9daea7a519e9d9a5fbfa639bc1840e
Douglas Duckworth <dod2014@med.cornell.edu>
2018-07-14 14:33:56 -0400

    apachemon - fixed syntax problems using flake8 - one should not simply pass exceptions - code now works in reliable way

--- apachemon.py
+++ apachemon.py
@@ -1,39 +1,42 @@
 #!/usr/bin/env python
 
-__author__ = 'quackmaster@protonmail.com'
+# TODO
+# 1) solve 'else' with hostb in ssh() as no exception occurs yet clause ran
+# 2) make this run as a daemon with something like supervisord
+# 3) make this work with more than two apache servers
 
-#TODO
-#1) make this run as a daemon with something like supervisord
-#2) make this work with more than two apache servers
+__author__ = 'quackmaster@protonmail.com'
 
 import pingdomlib
 import fabric
-import sys
 import urllib
+import sys
+from invoke import UnexpectedExit
 from slackclient import SlackClient
 
-# slack creds
-st = "oauthtoken"
+# slack creds - broken up due to length
+st1 = "token1"
+st2 = "token2"
+st = st1 + st2
 sc = SlackClient(st)
 
 # pingdom creds
-api = pingdomlib.Pingdom('user', 
-    'creds', 'creds')
+api = pingdomlib.Pingdom('login',
+                         'cred', 'cred')
 
-# checking for site using its pindgom id
-apache = api.getCheck(pingdom_check)
+# ssh user for fabric.Connection
+user = 'root'
 
-# find which apache server currently has the virtual ip
+# site we're monitoring via pingdom
+site = 'https://web.example.com'
+
+# defining site using its pindgom id
+apache = api.getCheck(1359632)
+
+# ssh() finds which apache server currently has the virtual ip
+
+
 def ssh():
-
-    # fabric keeps printing to stdout
-    # which is annoying so we're 
-    # disabling it entirely
-    class NullDevice():
-        def write(self, s):
-            pass
-
-    fabric.stdout = NullDevice()
 
     # below used in find_hosts()
     global hosta
@@ -44,51 +47,64 @@ def ssh():
 
     hosta = 'servera.example.com'
     hostb = 'serverb.example.com'
-    
-    c = '/usr/sbin/ip addr show dev ' 
+    c = '/usr/sbin/ip addr show dev '
     iface = 'em1:1'
-    grep =  " | grep 'inet ' | cut -d ' ' -f 6  | cut -f 1 -d '/' | grep " 
+    grep = " | grep 'inet ' | cut -d ' ' -f 6  | cut -f 1 -d '/' | grep "
     ip = '140.251.30.43'
 
     command = c + iface + grep + ip
-   
-    hosta_c = fabric.Connection(hosta)
-    hostb_c = fabric.Connection(hostb)
+    hosta_c = fabric.Connection(hosta, user=user)
+    hostb_c = fabric.Connection(hostb, user=user)
 
-    # make the ssh connections to both hosts
-    # if host does not have virtual ip
-    # then the variable below will not exist
-    # thus the need to pass exception
-    # if the ip does exist then a message
-    # will be printed to stdout so we have
-    # made that a NullDevice as shown above
+    # slack message sent if another exception occurs
+    # which will result in sys.exit()
+    # hosta_emsg = "error with ssh() to " + hosta
+    # hostb_emsg = "error with ssh() to " + hostb
+
+    # we must define None as ssh() will exit with Exception when host lacks IP
     try:
         hosta_r = hosta_c.run(command, hide=True)
-    except:
-       pass
-  
+    except UnexpectedExit:
+        hosta_r = None
+        pass
+#   # else:
+#   #     sc.api_call("chat.postMessage", channel="#pingdom", text=hosta_emsg)
+#   #     raise
+#   #     sys.exit()
+
     try:
         hostb_r = hostb_c.run(command, hide=True)
-    except:
-       pass
+    except UnexpectedExit:
+        hostb_r = None
+        pass
+    # else:
+    #    # print(hostb_r)
+    #    sc.api_call("chat.postMessage", channel="#pingdom", text=hostb_emsg)
+    #    sys.exit()
+
 
 ssh()
 
 # find out if services are down
-def check_outage():
-    
-    global downtime
 
-    #outages() prints all recent outages - only want last one
-    downtime = []
-    
+
+def check_outage():
+
+    global dt
+
+    # outages() prints all recent outages - only want last one
+    # so putting all in new list which will be used in flip_hosts()
+    dt = []
+
     if apache.status != 'up':
         for outage in apache.outages():
-        
+
             # append outages to new list
-            downtime.append("%d" % ((outage['timeto'] - outage['timefrom']) / 60))
-        
+            dt.append("%d" % ((outage['timeto'] - outage['timefrom']) / 60))
+
+
 check_outage()
+
 
 # this function builds list of defined apache servers
 # as well as the result of ssh()
@@ -103,23 +119,44 @@ def find_hosts():
     # hosts which lack ip will have None added to the right
     ah = [hosta, hostb]
 
-    # we must insert None as we passed exceptionin ssh()
-    # therefore, if hosta did not have the virtual ip
-    # then the variable hosta_r won't actually have anything
-    # so we populate it as None in the list
-    try:
-        har = (hosta_r).stdout
-        ah.insert(1, har)
-    except:
-        ah.insert(1, None)
-        pass
+    # host[a:b]_r will always exist given exception handling in ssh()
+    # though will make sure here as well
+    # stdout will not exist if var happens to be None
+    # so we are allowing it to pass if that's the case
 
     try:
+        hosta_r
+        har = (hosta_r).stdout
+        if type(har) is unicode:
+            ah.insert(1, str(har).strip('[]').strip('\n'))
+        else:
+            print('hosta_r error')
+            sys.exit()
+    except (NameError, AttributeError):
+        if hosta_r is None:
+            har = hosta_r
+            ah.insert(1, har)
+            pass
+        else:
+            print('hosta_r error')
+            sys.exit()
+
+    try:
+        hostb_r
         hbr = (hostb_r).stdout
-        ah.insert(3, hbr)
-    except:
-        ah.insert(3, None)
-        pass
+        if type(hbr) is unicode:
+            ah.insert(3, str(hbr).strip('[]').strip('\n'))
+        else:
+            print('hostb_r error')
+            sys.exit()
+    except (NameError, AttributeError):
+        if hostb_r is None:
+            hbr = hostb_r
+            ah.insert(3, hbr)
+            pass
+        else:
+            print('hostb_r error')
+            sys.exit()
 
     # find if hosta or hostb are active host
     # at time of reported outage
@@ -128,79 +165,74 @@ def find_hosts():
     # meaning the host which will lose the ip
     # we also define the opposite as newhost
 
-    if ah[0] is hosta:
-        if ah[1] is None:
-            if ah[2] is hostb:
-                if ah[3] == hbr:
-                    fliphost = hostb
-                    newhost = hosta
+    # print(isinstance(ah[3], str))
+
+    if ah[0] is hosta and ah[2] is hostb:
+        if ah[1] is None and ah[3] == ip:
+            fliphost = hostb
+            newhost = hosta
+
         else:
-            if ah[2] is hostb:
-                if ah[1] == har:
-                    if ah[3] is None:
-                        fliphost = hosta
-                        newhost = hostb
+            if ah[1] == ip and ah[3] is None:
+                fliphost = hosta
+                newhost = hostb
+            else:
+                print("unknown error")
+                sys.exit()
+
 
 find_hosts()
 
 # run from mgmt host which has ssh key access
+
+
 def flip_hosts():
 
-    dt = downtime[-1]
+    # we want last downtime in dt[]
+    ldt = dt[-1]
 
-    # slack messages sent 
-    global emsg
+    # slack messages sent
+    global e
     global not200
-    emsg = fliphost + ' ' + apache.status + ' gt 10 m - dt ' + dt + ' - ' + newhost + ' active'
-    npmsg = fliphost + ' ' + apache.status + ' lt 10 m' + ' please investigate or wait longer'
-    not200 = emsg + ' though does not return 200 so urgently investigate'
-    
+    e = fliphost + ' ' + apache.status + ' ' + ldt + ' >10 ' + newhost + ' up'
+    np = fliphost + ' ' + apache.status + ' <10 ' + ' please investigate'
+    not200 = e + ' though does not return 200 so urgently investigate'
+
     # variables
     ifup = 'ifup em1:1 && sleep 2'
     ifdown = 'ifdown em1:1 && sleep 2'
     rapache = '/usr/sbin/apachectl graceful'
-    user = 'root'
 
     if apache.status == 'up':
         pass
 
     else:
-        if downtime[-1] >= 10:
-            if fliphost is hosta: 
+        if ldt >= 10:
+            if fliphost is hosta:
                 fabric.Connection(hosta, user=user).run(ifdown)
                 fabric.Connection(hostb, user=user).run(ifup)
                 fabric.Connection(hostb, user=user).run(rapache)
-            if fliphost is hostb: 
+            if fliphost is hostb:
                 fabric.Connection(hostb, user=user).run(ifdown)
                 fabric.Connection(hosta, user=user).run(ifup)
                 fabric.Connection(hosta, user=user).run(rapache)
         else:
-            sc.api_call(
-             "chat.postMessage",
-              channel="#pingdom",
-              text=npmsg
-             )
+            sc.api_call("chat.postMessage", channel="#pingdom", text=np)
+
+
 flip_hosts()
+
 
 def check_site():
 
     # urllib get http status code
     # then send message if site back
-    site = 'https://site.you.want.to.monitor'
     code = urllib.urlopen(site).getcode()
 
     if code == 200:
-            sc.api_call(
-             "chat.postMessage",
-              channel="#pingdom",
-              text=emsg
-             )
-    else: 
-        sc.api_call(
-         "chat.postMessage",
-          channel="#pingdom",
-          text=not200
-         )
+            sc.api_call("chat.postMessage", channel="#pingdom", text=e)
+    else:
+        sc.api_call("chat.postMessage", channel="#pingdom", text=not200)
+
 
 check_site()
-
